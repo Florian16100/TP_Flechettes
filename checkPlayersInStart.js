60f65ba1c3db9ae642f979da1bc5a776372c4e59
Florian16100 <florian.lys@ynov.com>
2020-02-24 09:15:19 +0100

    engine

--- checkPlayersInStart.js
+++
@@ -1,8 +0,0 @@
-module.exports = function checkPlayersInStart (nombre) {
-  if(nombre > 2 && nombre < 5) {
-    return true;
-  } else {
-    return "Entrez un nombre entre 2 et 5, merci !";
-  }
-}
-
--- engine/engine.js
+++ engine/engine.js
@@ -1,75 +1,43 @@
 var inquirer = require('inquirer');
 
-console.log('Bienvenue sur la partie engine');
+console.log('Allons y !!');
+
+var nbPlayers = null;
 
 function checkPlayersInStart (nombre) {
-  if(nombre > 2 && nombre < 5) {
+  if(nombre > 1 && nombre < 5) {
     return true;
   } else {
-    return "Entrer un nombre compris entre 2 et 5";
+    return "Entrer un nombre compris entre 2 et 4";
   }
 }
 
-var questions = [
+var questionJoueurs = [
   {
     type: 'input',
     name: 'nbPlayers',
     message: 'Entrer le nombre de players',
     default: '2',
     validate: checkPlayersInStart,
-  },
-  {
-    type: 'input',
-    name: 'namePlayers',
-    message: 'Entrer le nom des joueurs',
   }
-];
+]
 
-inquirer.prompt(questions).then(answers => {
-  console.log(JSON.stringify(answers, null, '  '));
+var questionNomJoueurs = []
+
+inquirer.prompt(questionJoueurs).then(answers => {
+  nbPlayers = answers.nbPlayers;
+  for (var i = 0; i < nbPlayers; i++) {
+    questionNomJoueurs.push({
+      type: 'input',
+      name: 'namePlayers'+(i+1),
+      message: 'Entrer le nom du joueur'+(i+1)
+    })
+  }
+  inquirer.prompt(questionNomJoueurs).then(answers => {
+    console.log(answers)
+    for (var namePlayers in answers) {
+      // Créer des instances de la class Players
+      const player = new Players(name);
+    }
+  })
 });
-
-// selectNameJoueurs() {
-//   inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       message: 'Entrer le nom des joueurs',
-//       name: 'namePlayers',
-//     }
-//   ])
-//   .then(answers => {
-//     nomsjoueurs.push(answers.namePlayers)
-//     console.log(nomsjoueurs)
-//   })
-// }
-
-// //lancerFlechette(){
-//     inquirer
-//     .prompt([
-//       {
-//         type: 'input',
-//           name: 'fleche1',
-//           message: 'Entrer le nombre que vous venez de faire :',
-//       },
-//       {
-//         type: 'input',
-//           name: 'fleche2',
-//           message: 'Entrer le nombre que vous venez de faire :',
-//       },
-//       {
-//         type: 'input',
-//           name: 'fleche3',
-//           message: 'Score du joueur',
-//       },
-//     ])
-//   }
-
-//   run() {
-//   this.selectJoueurs(this.selectNameJoueurs);
-//   this.selectJoueurs(this.selectlancerFlechette);
-//   }
-
-
-// const test = new Game()
-// test.run()
