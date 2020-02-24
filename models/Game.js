fc4e435883b5e0083988d5185825e29a864eac73
Florian16100 <florian.lys@ynov.com>
2020-02-24 19:15:22 +0100

    models

---
+++ models/Game.js
@@ -0,0 +1,6 @@
+class Game extends gamemode {
+    constructor(mode,status) {
+        this.mode = ["around-the-world", "301", "cricket"];
+        this.status = ["draft", "started", "ended"];
+    }
+}
